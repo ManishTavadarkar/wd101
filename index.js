36f17a89bbf0b30656d62658d40d68e79c19b4e3
ManishTavadarkar <127777885+ManishTavadarkar@users.noreply.github.com>
2023-03-14 00:37:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,63 +1,92 @@
-<!DOCTYPE html>
-<html lang="en">
+let userform  = document.getElementById("user-form");
+
+const retrieveEntries = () => {
+    let entries = localStorage.getItem("user-entries");
+    if (entries){
+        entries = JSON.parse(entries);
+    }
+    else{
+        entries = [];
+
+    }
+    return entries;
+    }
+    let userEntries = retrieveEntries ();
+    
+    const displayEntries = () => {
+            const entries = retrieveEntries();
+            let table_entries = entries.map((entry) => {
+               const nameCell = `<td class='border px-4 py-2'> ${entry.name}</td>` ;
+               const emailCell = `<td class='border px-4 py-2'> ${entry.email}</td>` ;
+               const passwordCell = `<td class='border px-4 py-2'> ${entry.password}</td>` ;
+               const dobCell = `<td class='border px-4 py-2'> ${entry.dob}</td>` ;
+               const acceptTermsCell = `<td class='border px-4 py-2'>${entry.acceptedTermsAndConditions}</td>`;
+
+               const row = `<tr>${nameCell} ${emailCell} ${passwordCell} ${dobCell} ${acceptTermsCell}</tr>`;
+
+               return row;
+
+            }).join("\n");
+
+            let table = `<table class="table-auto w-full" style=" border: 1px solid;
+            margin: 0;"><tr>
+            
+            <th class="px-4 py-2">Name</th>
+            <th class="px-4 py-2">Email</th>
+            <th class="px-4 py-2">Password</th>
+            <th class="px-4 py-2">dob</th>
+            <th class="px-4 py-2">accepted terms?</th>
+</tr>${table_entries}</table>`;
+
+let details = document.getElementById("table");
+details.innerHTML = table;
+
+}
+const saveUserForm = (event) => {
+    event.preventDefault();
+
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+    const dob = document.getElementById("dob").value;
+
+    const acceptedTermsAndConditions = document.getElementById("acceptTerms").checked;
+    
+    let currentyear = new Date().getFullYear();
+    let birthyear = dob.split("-");
+    let year = birthyear[0];
+
+    let age = currentyear - year;
+    let isValid = (age) => {
+        if (age > 55 || age < 18) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+    if (!isValid(age)) {
+        document.getElementById("dob").style = "border: 2px solid red;"
+        return alert("You must be between age 18 and 55.\nYou are not elligible!");
+    } else {
+        document.getElementById("dob").style = "border: 2px solid #cccc;"
+
+    const entry ={
+        name,
+        email,
+        password,
+        dob,
+        acceptedTermsAndConditions
+    };
+    userEntries.push(entry);
+
+    localStorage.setItem("user-entries",JSON.stringify(userEntries));
+    displayEntries();
+}
 
 
-      
-<head>
-      <meta charset="UTF-8">
-      <meta http-equiv="X-UA-Compatible" content="IE=edge">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>register</title>
-      <link rel="stylesheet" href="style.css">
-</head>
 
-      
-      <div class="outer-box" style="width: 100vw;height: 200vh;">
-      <div class="inner-box" style="margin: 1 auto; border-radius: 11px;">
-      <header class="signup-header">
-      <h1 style="background-color: pink;font-style: italic;">Registeration Form</h1>
-      </header>
-      <main class="signup-body">
+}
 
-<form id="user-form">
-      <p>
-    <label for="name">Name<span class="red">*</span> :</label>
-    <input required type="text" id="name" name="name" placeholder="enter your name here ">
-      </p>
-      <p>
-    <label for="email">Email</label>
-    <input required type="email" id="email" name="email" placeholder="enter your email here " >
-      </p>
-      <p>
-    <label for="password">Password</label>
-    <input required type="password" id="password" name="password">
-      </p>
-      <p>
-    <label for="dob">Date of Birth</label>
-    <input required type="date" id="dob" name="dob">
-      </p>
-      <p>
-    <input required  type="checkbox" id="acceptTerms" name="acceptTerms">
-    <label for="acceptTerms">Accept Terms & Conditions</label>
-      </p>
-      <p class="submit">
-    <button type="submit">Submit</button>
-    </p>
- </form>
-</main>
-</div>
-<div class="circle c1">
 
-</div>
-<div class="circle c2"></div>
-<div class="entries">
-    <h3 class="center"><span>Entries</span></h3>
-    <div id="table"></div>
-</div>
-</div>
- <div id="user-entries"></div>
- <script src="index.js"></script>
-
-</body>
-
-</html>
+    userform.addEventListener("submit", saveUserForm);
+    displayEntries();
